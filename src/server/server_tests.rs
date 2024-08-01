60bd2ea2657490c7126313a41a28e92ce4d3a530
Suranjan <7suranjanpoudel7@gmail.com>
2024-08-01 10:50:43 +0100

    more tests for satisfaction

--- src/server/server_tests.rs
+++ src/server/server_tests.rs
@@ -1,6 +1,7 @@
 #[cfg(test)]
 mod tests {
     use crate::server::ws_server::WSServer;
+    use crate::ws_core::ws_errors::HTTPStatus;
 
     #[test]
     fn test_ws_handshake() {
@@ -34,5 +35,95 @@ mod tests {
             .to_ascii_lowercase();
 
         assert_eq!(expected_res_handshake, actual_handskhake);
+    }
+
+    #[test]
+    fn test_ws_invalid_http_version() {
+        let handshake = "GET ws://echo.websocket.org/ HTTP/1.0\n\
+                Host: echo.websocket.org\n\
+                Connection: Upgrade\n\
+                Pragma: no-cache\n\
+                Cache-Control: no-cache\n\
+                Upgrade: websocket\n\
+                Origin: https://websocketking.com\n\
+                Sec-WebSocket-Version: 13\n\
+                Accept-Encoding: gzip, deflate, br, zstd\n\
+                Sec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ==\n\
+                Sec-WebSocket-Extensions: permessage-deflate; client_max_window_bits";
+
+        let mut ws_server = WSServer::new();
+
+        validate_400_error(handshake, &mut ws_server, 400, HTTPStatus::BadRequest);
+    }
+
+    #[test]
+    fn test_ws_invalid_key() {
+        let handshake = "GET ws://echo.websocket.org/ HTTP/1.0\n\
+                Host: echo.websocket.org\n\
+                Connection: Upgrade\n\
+                Pragma: no-cache\n\
+                Cache-Control: no-cache\n\
+                Upgrade: websocket\n\
+                Origin: https://websocketking.com\n\
+                Sec-WebSocket-Version: 13\n\
+                Accept-Encoding: gzip, deflate, br, zstd\n\
+                Sec-WebSocket-Key: YWJj\n\
+                Sec-WebSocket-Extensions: permessage-deflate; client_max_window_bits";
+
+        let mut ws_server = WSServer::new();
+
+        validate_400_error(handshake, &mut ws_server, 400, HTTPStatus::BadRequest);
+    }
+
+    #[test]
+    fn test_ws_missing_headers() {
+        let handshake = "GET ws://echo.websocket.org/ HTTP/1.0\n\
+                Host: echo.websocket.org\n\
+                Pragma: no-cache\n\
+                Cache-Control: no-cache\n\
+                Upgrade: websocket\n\
+                Origin: https://websocketking.com\n\
+                Sec-WebSocket-Version: 13\n\
+                Accept-Encoding: gzip, deflate, br, zstd\n\
+                Sec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ==\n\
+                Sec-WebSocket-Extensions: permessage-deflate; client_max_window_bits";
+
+        let mut ws_server = WSServer::new();
+
+        validate_400_error(handshake, &mut ws_server, 400, HTTPStatus::BadRequest);
+    }
+
+    #[test]
+    fn test_ws_wrong_http_method() {
+        let handshake = "GET ws://echo.websocket.org/ HTTP/1.1\n\
+                Host: echo.websocket.org\n\
+                Connection: Upgrade\n\
+                Pragma: no-cache\n\
+                Cache-Control: no-cache\n\
+                Upgrade: websocket\n\
+                Origin: https://websocketking.com\n\
+                Sec-WebSocket-Version: 13\n\
+                Accept-Encoding: gzip, deflate, br, zstd\n\
+                Sec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ==\n\
+                Sec-WebSocket-Extensions: permessage-deflate; client_max_window_bits";
+
+        let mut ws_server = WSServer::new();
+
+        validate_400_error(handshake, &mut ws_server, 405, HTTPStatus::MethodNotAllowed);
+    }
+
+    fn validate_400_error(
+        handshake: &str,
+        ws_server: &mut WSServer,
+        http_code: u16,
+        status: HTTPStatus,
+    ) {
+        match ws_server.parse_client_handshake(handshake.as_bytes().to_vec()) {
+            Ok(()) => (),
+            Err(e) => {
+                assert_eq!(e.code, http_code);
+                assert_eq!(e.status, status);
+            }
+        }
     }
 }
--- src/ws_core/ws_errors.rs
+++ src/ws_core/ws_errors.rs
@@ -1,7 +1,7 @@
 use std::fmt;
 use std::fmt::Formatter;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Eq, PartialEq)]
 pub enum HTTPStatus {
     BadRequest,
     Unauthorised,
